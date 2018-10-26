519a7b86ec9e9fe71a2dd614e88fdef00f1c0131
Linuskarlsson42 <linus.che@hotmail.com>
2018-10-26 09:45:17 +0200

    commit
    skapat de tre första uppgifterna

--- src/fysik_uppgift/table.java
+++ src/fysik_uppgift/table.java
@@ -5,11 +5,20 @@ import java.util.Scanner;
 public class table {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+//		uppgift 1 
 		
-		double volume = input.nextDouble();
+		//double volume = 80;
+		//System.out.println(bibliotek.volumeToMass(SolidTable.IRON, volume));
 		
-		System.out.println(bibliotek.volumeToMass(SolidTable.IRON, volume));
-		
+//		uppgift 2
+		double velocity = 2.8;
+		double time = 60*60;
+		System.out.println(bibliotek.svtDistance(velocity, time));
+//		uppgift 3
+		double volume = 5;
+		double deltaT = 1;
+		System.out.println(bibliotek.heat(FluidTable.VATTEN, volume, deltaT));
+//		uppgift 4
 	}
 
 }
